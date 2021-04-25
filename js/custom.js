901714ef0b1455df4f7240201bd9aebbc2f512b5
Teddy Matayoshi <teddymatayoshi@users.noreply.github.com>
2021-04-25 17:48:29 +0900

    add article-header class

--- js/custom.js
+++ js/custom.js
@@ -1,2 +1,2 @@
-/*Change home page body css element from home-template to post-template */
+/* Change home page body css element from home-template to post-template */
 document.getElementsByTagName("body")[0].setAttribute("class", "post-template");
