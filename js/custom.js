7f742d9cf0c650642699a1a5592d46e8e723bc87
Teddy Matayoshi <teddymatayoshi@users.noreply.github.com>
2021-04-25 19:47:49 +0900

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,2 +1,5 @@
 /* Change home page body css element from home-template to post-template */
 document.getElementsByTagName("body")[0].setAttribute("class", "post-template");
+
+/* Change text */
+String.replace(/Powered by Ghost/gi, '');
