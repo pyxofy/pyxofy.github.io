59f5d76585e5c27598f66823af9646b52c3a55ea
Teddy Matayoshi <teddymatayoshi@users.noreply.github.com>
2021-04-25 20:07:37 +0900

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,2 @@
 /* Change home page body css element from home-template to post-template */
 document.getElementsByTagName("body")[0].setAttribute("class", "post-template");
-
-/* Replace text */
-let textToReplace = document.querySelector("a");
-let classess = textToReplace = a.classList;
-let result = classess.replace = ("Powered by Ghost", "");
-console.log(result);
