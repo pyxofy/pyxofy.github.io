2b54a1960fc7fe214b15df54d4eede3976251114
Teddy Matayoshi <teddymatayoshi@users.noreply.github.com>
2021-04-26 19:10:00 +0900

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -5,14 +5,14 @@ document.getElementsByTagName("body")[0].setAttribute("class", "post-template");
 function deleteText1() {
 	let str = document.getElementsByClassName("post-template")[0];
 	let x = str.getElementsByTagName("a");
-	for (var i = 0; i < x.length; i++) {
-		var res = x[i].innerHTML.replace(/Powered by Ghost/gi, "");
+	for (let i = 0; i < x.length; i++) {
+		let res = x[i].innerHTML.replace(/Powered by Ghost/gi, "");
 		x[i].innerHTML = res;
 	}
     let str1 = document.getElementsByClassName("post-template")[0];
     let y = str1.getElementsByTagName("span");
-    for (var i = 0; i < y.length; i++) {
-		var res1 = y[i].innerHTML.replace(/Publish with Ghost/gi, "");
+    for (let i = 0; i < y.length; i++) {
+		let res1 = y[i].innerHTML.replace(/Publish with Ghost/gi, "");
 		y[i].innerHTML = res1;
 	}
 }
