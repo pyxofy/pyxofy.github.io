0de690b403eaf8b855ad095b5eca3365373262d3
Teddy Matayoshi <teddymatayoshi@users.noreply.github.com>
2021-04-26 19:30:35 +0900

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,22 +1,12 @@
 /* Change home page body css element from home-template to post-template */
 document.getElementsByTagName("body")[0].setAttribute("class", "post-template");
 
-/* Delete Text 1 */
-function deleteText1() {
+/* Delete Text */
+function deleteText() {
 	let str = document.getElementsByClassName("post-template")[0];
 	let x = str.getElementsByTagName("a");
 	for (let i = 0; i < x.length; i++) {
 		let res = x[i].innerHTML.replace(/Powered by Ghost/gi, "");
-		x[i].innerHTML = res;
-	}
-}
-
-/* Delete Text 2 */
-function deleteText2() {
-	let str = document.getElementsByClassName("gh-portal-powered")[0];
-	let x = str.getElementsByTagName("span");
-	for (let i = 0; i < x.length; i++) {
-		let res = x[i].innerHTML.replace(/Publish with Ghost/gi, "");
 		x[i].innerHTML = res;
 	}
 }
@@ -31,6 +21,5 @@ function replaceText() {
 	}
 }
 
-window.addEventListener("load", deleteText1);
+window.addEventListener("load", deleteText);
 window.addEventListener("load", replaceText);
-window.addEventListener("load", deleteText2);
