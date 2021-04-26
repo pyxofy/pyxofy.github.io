3a06bc89bac6b3637375d4904c7bec95fc6752b3
Teddy Matayoshi <teddymatayoshi@users.noreply.github.com>
2021-04-26 18:55:48 +0900

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -3,6 +3,7 @@ document.getElementsByTagName("body")[0].setAttribute("class", "post-template");
 
 /* Delete Text 1 */
 function deleteText1() {
+	let str = document.getElementsByClassName("main")[0];
 	let x = str.getElementsByTagName("a");
 	for (var i = 0; i < x.length; i++) {
 		var res = x[i].innerHTML.replace(/Powered by Ghost/gi, "");
