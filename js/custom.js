9acdcc19027d0841a8f0d4cd82060737f1f2e8fe
Teddy Matayoshi <teddymatayoshi@users.noreply.github.com>
2021-04-26 18:55:45 +0900

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,2 +1,13 @@
 /* Change home page body css element from home-template to post-template */
 document.getElementsByTagName("body")[0].setAttribute("class", "post-template");
+
+/* Delete Text 1 */
+function deleteText1() {
+	let x = str.getElementsByTagName("a");
+	for (var i = 0; i < x.length; i++) {
+		var res = x[i].innerHTML.replace(/Powered by Ghost/gi, "");
+		x[i].innerHTML = res;
+	}
+}
+
+window.addEventListener("load", deleteText1);
